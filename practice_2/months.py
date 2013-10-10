a4ea5eb1f4e7915c1165cfbebb63cb6694f3031e
Konstantin Ivanoff <kotanoff@mail.ru>
2013-10-11 01:58:59 +0400

    Added months.py

---
+++ practice_2/months.py
@@ -0,0 +1,6 @@
+# -*- encoding: utf-8 -*-
+m = input("Enter month: ")
+d = {"January": "1", "February":"2", "March":"3", "April":"4", "May":"5",
+     "June":"6","July":"7", "August":"8", "September":"9", "October":"10",
+     "November":"11", "Decemeber":"12"}
+print d[m]
