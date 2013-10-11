9ac9753889968151d9b1b86eac2b6077d24e965a
Konstantin Ivanoff <kotanoff@mail.ru>
2013-10-11 11:01:42 +0400

    Updated constants.py

--- practice_2/constants.py
+++ practice_2/constants.py
@@ -1,10 +1,19 @@
 # -*- encoding: utf-8 -*-
 import math
 def temp():
-    str = input("Введите строку вида '<имя константы:точность>: ")
     dic = {"pi":math.pi, "e":math.e}
+    str = input("Введите строку вида '<имя константы:точность>: ")
     a=str[:str.find(":")]
-    b=int(str[str.find(":")+1:])
-    print(round(dic[a],b))
+    try:
+        b=int(str[str.find(":")+1:])
+    except:
+        print('''Второй параметр - не число.
+По умолчанию установлена точность: 2''')
+        b=2
+    if(dic.has_key(a) and b >= 0):
+        print "%.{0}f".format(b) % dic[a]
+        #print(round(dic[a],b))
+    else:
+        print "Ошибочка вышла"
     
 temp()
