70cc3223211ff4ffc92c7f84c06958d33fbfd5ab
Konstantin Ivanoff <kotanoff@mail.ru>
2013-10-11 11:10:28 +0400

    Updated months.py. Added a check on a correct input

--- practice_2/months.py
+++ practice_2/months.py
@@ -3,4 +3,7 @@ m = input("Enter month: ")
 d = {"January": "1", "February":"2", "March":"3", "April":"4", "May":"5",
      "June":"6","July":"7", "August":"8", "September":"9", "October":"10",
      "November":"11", "Decemeber":"12"}
-print d[m]
+if (d.has_key(m)):
+    print d[m]
+else:
+    print("Введите-ка реальный месяц, пожалуйста.")
