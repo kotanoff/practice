bb8b86fe555f489ede21c355deafe21e0c275bd5
Konstantin Ivanoff <kotanoff@mail.ru>
2013-11-02 12:16:09 +0400

    Fixed models.py due to travis

--- practice_4/library/books/models.py
+++ practice_4/library/books/models.py
@@ -6,7 +6,7 @@ import datetime
 class Author(models.Model):
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
-    email = models.EmailField(null = True, blank = True)
+    email = models.EmailField(null=True, blank=True)
 
     def __unicode__(self):
       return u'%s %s' % (self.first_name, self.last_name)
