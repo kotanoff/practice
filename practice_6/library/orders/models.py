3c3a0795c45bdef420f063fc73db3efbd942efe5
Konstantin Ivanoff <kotanoff@mail.ru>
2013-11-16 02:51:07 +0400

    Installed South and made some migrations

---
+++ practice_6/library/orders/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from books.models import *
+from datetime import datetime
+#from utils.models import TimeStampedModel
+
+
+# Create your models here.
+class Customer(models.Model):
+    firstName = models.CharField(max_length=32)
+    lastName = models.CharField(max_length=32)
+    address = models.TextField()
+    is_approved = models.BooleanField()
+    email = models.EmailField('Email')
+
+    def __unicode__(self):
+        return u'%s %s' % (
+            self.lastName, self.firstName)
+
+
+class Order(models.Model):
+    itemId = models.ForeignKey(Book)
+    created = models.DateField(default=datetime.now())
+    customer = models.ForeignKey(Customer, null=True)
+		
