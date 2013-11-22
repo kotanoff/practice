dcbd10db656984e4359c9461ac099f64a05d9db2
Konstantin Ivanoff <kotanoff@mail.ru>
2013-11-22 04:33:53 +0400

    Task 2: registration and login. Also a prototype of site map.

--- practice_7/library/library/urls.py
+++ practice_7/library/library/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from orders.views import CustomersList, CustomerDetails
 from books.views import BookList, AuthorList, BookDetail, AuthorDetail
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,6 +9,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'^$', TemplateView.as_view(template_name="utils/map.html")),
     url(r'^library/$', BookList.as_view()),
     url(r'^library/books/$', BookList.as_view()),
     url(r'^library/books/(?P<pk>\d+)/$', BookDetail.as_view()),
@@ -15,6 +17,8 @@ urlpatterns = patterns(
     url(r'^library/authors/(?P<pk>\d+)/$', AuthorDetail.as_view()),
     url(r'^orders/$', CustomersList.as_view()),
     url(r'^orders/(?P<pk>\d+)/$', CustomerDetails.as_view(template_name="orders/customer.html")),
+    url(r'^registration/$', 'registration.views.registrate'),
+    url(r'^login/$', 'registration.views.log_in'),
     # Examples:
     # url(r'^$', 'imgs.views.home', name='home'),
     # url(r'^imgs/', include('imgs.foo.urls')),
